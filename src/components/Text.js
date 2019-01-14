85a2645fff899e4cabae8ff0a77663720859906b
Teerapat Tee <teerapat12345678@gmail.com>
2019-01-14 15:32:50 +0700

    redux

--- src/components/Text.js
+++ src/components/Text.js
@@ -8,37 +8,41 @@ import React, {Component} from 'react';
 import Service from '../service.js';
 import Show from './show';
 import Message from './message';
+import { connect } from 'react-redux'
+import { getDataCat } from "../redux/actions/cat";
 
 // Service.getImageItems(number)
 class Text extends Component {
         constructor(props) {
             super(props);
             this.state = {
-                value: '',
+                value: 0,
                 data: '',
                 message:''
             };
 
-            this.handleChange = this
-                .handleChange
-                .bind(this);
-            this.handleSubmit = this
-                .handleSubmit
-                .bind(this);
+            this.handleChange = this.handleChange.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
         }
 
-        handleChange(event) {
-                    this.setState({
-                                    value: event.target.value,
-                                    message:''
-                                     });
+        async componentWillMount() {
+            console.log("willmount Text.js");
+            this.setState({value: 4});
+
+        }
+        async handleChange(event) {
+            let value = await event.target.value
+            this.setState({value: value});
+            console.log(" value" + typeof this.state.value)
         }
 
         handleSubmit(event) {
-            
             Service.getImageItems(this.state.value)
-                // .then(results => console.log(results))       
-                .then(results => this.setState({data : results, message:'success'}))        
+                // .then(results => console.log(results))
+                this.props.getDataCat(this.state.value)
+                .then(results => {
+                    this.setState({data : results, message:'success'})
+                })
                 .catch(error => this.setState({message: 'Failed'}))
             event.preventDefault();
         }
@@ -54,13 +58,12 @@ class Text extends Component {
                                       </label>
                                                <input type="submit" value="Submit"/>
                                     </form>
-                                    {this.state.message =='Failed'
-                                        ?<div className="alert alert-danger"><Message message={this.state.message}/></div>
-                                        :<Show message={this.state.message} data={this.state.data}/>}
+                                        <Message />
+                                        <Show />
                                    
                              </div>
                                                        
-                      
+
                         
                     );
             
@@ -68,6 +71,19 @@ class Text extends Component {
         }
 }
 
-export default Text;
+// export default Text;
 
+
+const mapStateToProps = (state) => {
+    // console.log(state.user);
+    return {
+        cat: state.cat.data
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    getDataCat: (data) => dispatch(getDataCat(data))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Text)
 //  {this.state.data?<div><Show data={this.state.data}/></div>:<div><Message message="Loading..."/></div>}
--- src/components/show.js
+++ src/components/show.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
 import Message from './message';
+import { connect } from 'react-redux'
+
 class Show extends Component {
         render() {
-            const { data } = this.props
+            const { data, message } = this.props.cat
             return (
                  <div className="">
-
-                       {data && this.props.message=='success'
-                        ?<div className="alert alert-success"><Message message="success"/></div>
-                        :<div className="alert alert-warning"><Message message="Loading..."/></div>}
-                       <div className="row">
-                                    {data && data.map(d => {
-                                        return (
-                                            <div key={d.id} className="col-md-4">
-                                                            <img src={d.image} alt="Placeholder image" className="img-thumbnail"/>
-                                                            <div className="content">
-                                                                <h1>{d.label}</h1>
-                                                            </div>
-                                            </div>
-                                        )
-                                    })}
+                <div className="row">
+                    {data && data.map(d => {
+                        return (
+                            <div key={d.id} className="col-md-4">
+                                <img src={d.image} alt="Placeholder image" className="img-thumbnail"/>
+                                <div className="content">
+                                    <h1>{d.label}</h1>
+                                </div>
+                            </div>
+                        )
+                    })}
                     </div>
                 </div>
             )
         }
 }
 
-export default Show;
+
+const mapStateToProps = (state) => {
+    // console.log(state.user);
+    return {
+        cat: state.cat
+    }
+}
+
+// export default Show;
+export default connect(mapStateToProps)(Show)
